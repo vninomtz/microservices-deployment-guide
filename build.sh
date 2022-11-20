170ff24e27647a71ec156de222c4a1f14d90787d
Victor Niño <ninomtz.victor@gmail.com>
2022-11-20 11:54:59 -0600

    fix: args in mkdir command

--- build.sh
+++ build.sh
@@ -13,30 +13,25 @@ download_csl() {
 }
 
 join_content () {
-    mkdir "${BUILDDIR}" -p
+    mkdir -p "${BUILDDIR}"
     echo "Uniendo contenido"
     cat "${CONTENTDIR}/guia/introduccion.md" > "${PATHTMP}"
     echo "" >> "${PATHTMP}"
     cat "${CONTENTDIR}/guia/procesos.md" >> "${PATHTMP}"
-    # cat "./procesos/README.md" "./procesos/diseno.md" "./procesos/preparacion-plataforma.md" >> "${PATHTMP}"
     echo "" >> "${PATHTMP}"
     cat "${CONTENTDIR}/guia/diseno-despliegue.md" >> "${PATHTMP}"
-    # cat "./diseno-despliegue/README.md" "./diseno-despliegue/arquitectura.md" "./diseno-despliegue/estrategia.md" "./diseno-despliegue/tecnologias-infraestructura.md" "./diseno-despliegue/configuracion.md" "./diseno-despliegue/observabilidad.md" >> "${PATHTMP}"
     echo "" >> "${PATHTMP}"
     cat "${CONTENTDIR}/guia/admin-desarrollo.md" >> "${PATHTMP}"
-    # cat "./admin-entorno-desarrollo/README.md" "./admin-entorno-desarrollo/admin-config.md" "./admin-entorno-desarrollo/control-versiones.md" "./admin-entorno-desarrollo/admin-construccion.md" "./admin-entorno-desarrollo/admin-artefactos.md" >> "${PATHTMP}"
     echo "" >> "${PATHTMP}"
-    # cat "./pipeline-despliegue/README.md" "./pipeline-despliegue/integracion-continua.md" "./pipeline-despliegue/servidor-ci.md" "./pipeline-despliegue/automatizacion-liberaciones.md" "./pipeline-despliegue/entrega-continua.md" >> "${PATHTMP}"
     cat "${CONTENTDIR}/guia/pipeline.md" >> "${PATHTMP}"
     echo "" >> "${PATHTMP}"
     cat "${CONTENTDIR}/guia/infraestructura.md" >> "${PATHTMP}"
-    # cat "./infraestructura/README.md" "./infraestructura/admin-infra.md" "./infraestructura/infra-como-codigo.md" >> "${PATHTMP}"
     echo "" >> "${PATHTMP}"
     echo "# Referencias" >> "${PATHTMP}"
 }
 
 pdf_print() {
-    mkdir "${BUILDDIR}" -p
+    mkdir -p "${BUILDDIR}"
     echo "Creating pdf-print output"
     pandoc "${CONTENTDIR}/metadata.yaml" "${PATHTMP}" \
         --resource-path="${CONTENTDIR}" \
